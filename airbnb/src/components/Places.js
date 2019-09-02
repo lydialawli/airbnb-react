df60f494a64eea202a17e7bb1a494e4b01a69e72
Ly <lydialawli@gmail.com>
2019-09-02 13:19:17 +0700

    setup router page

---
+++ airbnb/src/components/Places.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Places extends React.Component {
+    
+    render() {
+        return (
+            <div>hey!</div>
+        )
+    }
+}
+
+export default Places
