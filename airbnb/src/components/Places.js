9896f5f02bd83fe4626e69f8fac1caa7a03968f8
Ly <lydialawli@gmail.com>
2019-09-05 17:10:17 +0700

    filters working on Places

--- airbnb/src/components/Places.js
+++ airbnb/src/components/Places.js
@@ -17,7 +17,8 @@ class Places extends React.Component {
                 stars: 4,
                 reviews: 37,
                 bg: 'https://q-ak.bstatic.com/images/hotel/max1024x768/186/186223203.jpg',
-                fav: false
+                fav: false,
+                rooms: 2,
             },
             {
                 title: 'Double Room Shared House',
@@ -27,7 +28,8 @@ class Places extends React.Component {
                 stars: 3,
                 reviews: 12,
                 bg: 'https://a0.muscache.com/4ea/air/v2/pictures/eee424d0-ca05-4405-8bdb-e5caf2db3fbe.jpg',
-                fav: false
+                fav: false,
+                rooms: 4,
 
             },
             {
@@ -38,7 +40,8 @@ class Places extends React.Component {
                 stars: 5,
                 reviews: 50,
                 bg: 'https://a0.muscache.com/4ea/air/v2/pictures/58f86a91-a526-4e1b-934e-8f6bc3f60e10.jpg',
-                fav: false
+                fav: false,
+                rooms: 3,
             },
             {
                 title: 'Studio Lounge small',
@@ -48,7 +51,8 @@ class Places extends React.Component {
                 stars: 5,
                 reviews: 4,
                 bg: 'https://q-ak.bstatic.com/images/hotel/max1024x768/186/186223203.jpg',
-                fav: false
+                fav: false,
+                rooms: 2,
             },
             {
                 title: 'Studio Lounge Big',
@@ -58,25 +62,39 @@ class Places extends React.Component {
                 stars: 2,
                 reviews: 36,
                 bg: 'https://a0.muscache.com/4ea/air/v2/pictures/eee424d0-ca05-4405-8bdb-e5caf2db3fbe.jpg',
-                fav: false
+                fav: false,
+                rooms: 1,
             },
             {
                 title: 'Single room private House',
-                description: 'Private house • 1 Rooms',
+                description: 'Private house • 6 Rooms',
                 price: 250,
                 id: 5,
                 stars: 4,
                 reviews: 43,
                 bg: 'https://q-ak.bstatic.com/images/hotel/max1024x768/186/186223203.jpg',
-                fav: false
+                fav: false,
+                rooms: 6,
+            },
+            {
+                title: 'Full Shared House',
+                description: 'Shared House • 10 Rooms',
+                price: 1000,
+                id: 7,
+                stars: 5,
+                reviews: 54,
+                bg: 'https://a0.muscache.com/4ea/air/v2/pictures/eee424d0-ca05-4405-8bdb-e5caf2db3fbe.jpg',
+                fav: false,
+                rooms: 8,
+
             },
         ],
         placeTitles: ['All types', 'Shared House', 'Shared Villa', 'Private Room', 'Shared room double bed'],
-        filterTypes: [{ value: 'date', name: 'Latest' }, { value: 'price', name: 'Price' }, { value: 'rating', name: 'Rating' }],
+        filterTypes: ['Latest', 'Price', 'Rating'],
         originalPlaces: []
     }
 
-    componentWillMount() {
+    UNSAFE_componentWillMount() {
         this.setState({
             originalPlaces: this.state.places
         })
@@ -98,6 +116,30 @@ class Places extends React.Component {
         this.setState({ places: filtered })
     }
 
+    filterByNumOfRooms = (e) => {
+        let rooms = Number(e.target.value) + 1
+        let filtered = this.state.originalPlaces.filter(e => e.rooms === rooms)
+
+        this.setState({ places: filtered })
+    }
+
+    filterByPrice = (e) => {
+        let maxPrice = e.target.value
+        let filtered = this.state.originalPlaces.filter(e => e.price <= maxPrice)
+
+        this.setState({ places: filtered })
+    }
+
+    sortBy = (e) => {
+        let type = e.target.value
+        let filtered = this.state.originalPlaces
+        if (type === 'Price')
+            filtered = this.state.originalPlaces.sort((a, b) => { return a.price - b.price })
+        else if (type === 'Rating')
+            filtered = this.state.originalPlaces.sort((a, b) => { return b.stars - a.stars })
+        else { filtered = this.state.originalPlaces.sort((a, b) => { return a.id - b.id })}
+        this.setState({ places: filtered })
+    }
 
 
     render() {
@@ -107,17 +149,17 @@ class Places extends React.Component {
                     <Nav></Nav>
                 </div>
                 <div className="filters">
-                    <select>
-                        {[...Array(10)].map((n, i) => { return <option key={i} value="1">Rooms: {i + 1}</option> })}
+                    <select onChange={(e) => this.filterByNumOfRooms(e)}>
+                        {[...Array(10)].map((n, i) => { return <option key={i} value={i}>Rooms: {i + 1}</option> })}
                     </select>
                     <select>
                         {this.state.placeTitles.map((e, i) => { return <option key={i} value="1">{e}</option> })}
                     </select>
-                    <input type="number" placeholder="max price" />
-                    <select>
-                        {this.state.filterTypes.map((e,i) => { return <option key={i} value={e.value}>{e.name}</option> })}
+                    <input onChange={(e) => this.filterByPrice(e)} type="number" placeholder="max price" />
+                    <select onChange={(e) => this.sortBy(e)}>
+                        {this.state.filterTypes.map((e, i) => { return <option key={i} value={e}>{e}</option> })}
                     </select>
-                    <input className='search' type='text' onChange={this.filterPlaces} placeholder={'Search...'}/>
+                    <input className='search' type='text' onChange={this.filterPlaces} placeholder={'Search...'} />
                 </div>
                 <div className="grid five large">
                     {this.state.places.map((p, i) => {
