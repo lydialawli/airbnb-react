c1daf108df19197a6c2ba6b6838c8ee74a94b3ab
lydialawli <lydialawli@gmail.com>
2019-10-24 20:00:43 +0200

    nothing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,21 +2,24 @@ import React from 'react'
 import '../styles/nav.css'
 import '../styles/buttons.css'
 import '../styles/users.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 class Nav extends React.Component {
     state = {
-        logo : '../logo-airbnb.png',
-        user: {avatar: 'https://seakoala.io/docs/images/universeLy.png', name: 'Ly'}
+        logo: '../logo-airbnb.png',
+        user: {
+            avatar: 'https://seakoala.io/docs/images/universeLy.png',
+            name: 'Ly'
+        }
     }
 
     render() {
         return (
             <nav>
-                <Link to="/" className="logo" style={{backgroundImage: `url(${this.state.logo})` }}></Link>
+                <Link to="/" className="logo" style={{ backgroundImage: `url(${this.state.logo})` }}></Link>
                 <div className="profile">
                     <Link to="/profile" className="button">
-                        <div className="avatar" style={{backgroundImage: `url(${this.state.user.avatar})` }}></div>
+                        <div className="avatar" style={{ backgroundImage: `url(${this.state.user.avatar})` }}></div>
                         <span>{this.state.user.name}</span>
                     </Link>
                 </div>
