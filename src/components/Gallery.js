ad51cf4b8359367ba07c8890545f3d4bda6714f9
lydialawli <lydialawli@gmail.com>
2019-10-28 21:00:58 +0100

    trying to fix BigImageee

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -7,56 +7,56 @@ import '../styles/buttons.css'
 
 class Gallery extends React.Component {
     state = {
-		user: this.props,
-		place: this.props.place,
+        user: this.props,
+        place: this.props.place,
         liked: '',
-        bigImage: this.props.place.images[0],
-	}
+        bigImage: '',
+    }
 
-	UNSAFE_componentWillReceiveProps(props) {
-		let liked = this.state.liked
-		
-		if (props.user.likes.includes(props.place._id)) {
-			liked = true
-		}
-		else { liked = false }
+    UNSAFE_componentWillReceiveProps(props) {
+        let liked = this.state.liked
 
-		this.setState({
-			place: props.place,
+        if (props.user.likes.includes(props.place._id)) {
+            liked = true
+        }
+        else { liked = false }
+
+        this.setState({
+            place: props.place,
             user: props.user,
             biImage: props.place.images[0],
-			liked
-		})
-	}
-
-	componentDidMount() {
-		let user = this.state.user.user
-		if (user.likes.includes(this.state.place._id))
-			this.setState({ liked: true })
-	}
-
-	like = (e) => {
-		e.preventDefault()
-		this.props.like(this.state.place._id)
+            liked
+        })
     }
-    
+
+    componentDidMount() {
+        let user = this.state.user.user
+        if (user.likes.includes(this.state.place._id))
+            this.setState({ liked: true, bigImage: this.props.place.images[0] })
+    }
+
+    like = (e) => {
+        e.preventDefault()
+        this.props.like(this.state.place._id)
+    }
+
     changeBigImage = (i) => {
         this.setState({
             bigImage: this.state.place.images[i]
         })
     }
-  
+
     render() {
         return (
             <div className="gallery">
                 <div className="image-main" style={{ backgroundImage: `url(${this.state.bigImage})` }}>
-                    <button className="icon" type="button" onClick={(e) => {this.like(e)}}>
+                    <button className="icon" type="button" onClick={(e) => { this.like(e) }}>
                         <i className={this.state.liked ? "fas fa-heart" : "far fa-heart"}></i>
                     </button>
                 </div>
                 <div className="thumbnails">
                     {this.state.place.images.map((e, i) => {
-                        return <div key={i} className={`thumbnail ${this.state.bigImage === e ? 'selected' : ''}`} onClick={()=> this.changeBigImage(i)} style={{ backgroundImage: `url(${this.state.place.images[i]})` }}></div>
+                        return <div key={i} className={`thumbnail ${this.state.bigImage === e ? 'selected' : ''}`} onClick={() => this.changeBigImage(i)} style={{ backgroundImage: `url(${this.state.place.images[i]})` }}></div>
                     })}
 
                 </div>
